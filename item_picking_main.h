dbf47b5ea62b222b0ea2c87493f10a1241a44bac
Kalle Karhu <kalle.karhu@iki.fi>
2019-12-26 06:10:48 -0800

    and the missing files

---
+++ item_picking_main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "item_picker.h"
+#include "priest_character.h"
+
+namespace css
+{
+
+int ItemPickingMain(int argc, char** argv, ItemPicker::ValueChoice value_choice, const PriestCharacter& c);
+
+}  // namespace ccs
+  
