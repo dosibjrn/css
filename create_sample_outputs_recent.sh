ecb45612a50da89002e5744fd2e868d8815ebdae
Kalle Karhu <kalle.karhu@iki.fi>
2020-05-28 08:28:21 +0300

    initial sample output update for the recent run stuff

--- create_sample_outputs_recent.sh
+++ create_sample_outputs_recent.sh
@@ -7,4 +7,7 @@ for instance in bwl mc onyxia zg; do
   ./css 4 ../items/more_raid.csv ../items/not_for_priests.txt no-locks recent_${instance}_bis -a ../confs/bwl_2020-04-09.csv -l /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Logs/recent/$instance.txt | tee ../sample_output/log_based/recent_${instance}_bis.txt
 done
 
+mkdir ../sample_output/tags/
+cp *.pawn_tag.txt ../sample_output/tags/. 
+tail -n 1 *.pawn_tag.txt > ../sample_output/tags/all_latest_tags.txt
 
