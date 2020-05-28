543c05f3ea97295faad90e3020b6afe9a9541ea4
Kalle Karhu <kalle.karhu@iki.fi>
2020-05-28 08:16:05 +0300

    added some recent runs and a script for running for those

---
+++ create_sample_outputs_recent.sh
@@ -0,0 +1,10 @@
+# Run from e.g. css/build or css/bin
+
+cp ../items/have_mod_locked.txt start_with.txt
+
+for instance in bwl mc onyxia zg; do
+  ./css 4 ../items/more_raid.csv ../items/not_for_priests.txt start_with.txt recent_${instance}_current -a ../confs/bwl_2020-04-09.csv -l /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Logs/recent/$instance.txt | tee ../sample_output/log_based/recent_${instance}_current.txt
+  ./css 4 ../items/more_raid.csv ../items/not_for_priests.txt no-locks recent_${instance}_bis -a ../confs/bwl_2020-04-09.csv -l /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Logs/recent/$instance.txt | tee ../sample_output/log_based/recent_${instance}_bis.txt
+done
+
+
